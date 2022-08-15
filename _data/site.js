78efd71dbedc53244653dba26c87c152239d03a8
Shweata N Hegde <shweata.hegde@gmail.com>
2022-08-15 09:20:00 +0530

    removed placeholders; added our content

--- _data/site.js
+++ _data/site.js
@@ -1,20 +1,21 @@
 module.exports = {
   meta: {
-    title: "My Purple Blog",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    title: "Climate Knowledge Hunt",
+    description: "Hackathon to make sense of climate literature",
     lang: "en",
-    siteUrl: "https://example.com/",
+    siteUrl: "https://climate-knowledge-hunt.netlify.app/",
   },
   feed: { // used in feed.xml.njk
     subtitle: "Lorem ipsum dolor sit amet consecuteor",
     filename: "atom.xml",
     path: "/atom.xml",
-    id: "https://example.com/",
-    authorName: "John Doe",
-    authorEmail: "johndoe@example.com"
+    id: "https://climate-knowledge-hunt.netlify.app/",
+    authorName: "Semantic Climate",
+    authorEmail: ""
   },
   hero: { // used in hero section of main page ie. index.html.njk
-    title: "Welcome to my purple blog",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusantium deserunt odio esse."
+    title: "First season of Climate Knowledge Hunt on 24th September 2022",
+    description: "to join us!",
+    reglink: "reglink"
   }
 }
---
+++ pages/team.md
@@ -0,0 +1,9 @@
+---
+title: Who are we?
+description: Learn more about team
+layout: layouts/post.njk
+hideTagsList: true
+eleventyNavigation:
+  key: Team
+  weight: 4
+---
