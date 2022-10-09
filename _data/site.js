bbaf8089d0398d9bc18e9cc3d0ddc4f96f47cfea
Shweata N Hegde <shweata.hegde@gmail.com>
2022-10-09 11:07:30 +0530

    site name change

--- _data/site.js
+++ _data/site.js
@@ -1,20 +1,20 @@
 module.exports = {
   meta: {
-    title: "SemanticClimate",
+    title: "#semanticClimate",
     description: "Liberating knowledge from climate-related reports",
     lang: "en",
-    siteUrl: "https://climate-knowledge-hunt.netlify.app/",
+    siteUrl: "https://semanticclimate.netlify.app/",
   },
   feed: { // used in feed.xml.njk
     subtitle: "Lorem ipsum dolor sit amet consecuteor",
     filename: "atom.xml",
     path: "/atom.xml",
-    id: "https://climate-knowledge-hunt.netlify.app/",
+    id: "https://semanticclimate.netlify.app/",
     authorName: "Semantic Climate",
     authorEmail: ""
   },
   hero: { // used in hero section of main page ie. index.html.njk
-    title: "Semantic Climate",
+    title: "#semanticClimate",
     description: "",
   }
 }
