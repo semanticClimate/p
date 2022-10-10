8ee2af7edc63095e9f5aa3423342938f4fdde13b
Axel Dürkop <axel.duerkop@tuhh.de>
2022-10-10 12:26:52 +0200

    Add suggestion for about sentence

--- _data/site.js
+++ _data/site.js
@@ -7,20 +7,24 @@ module.exports = {
   },
   homepage: {
     sectionHeading1: "What's #semanticClimate?",
-    description1: "We are a team of... (link the about and team page)",
+    description1:
+      "We are a team of scientists and enthusiasts who liberate knowledge from climate-related reports.",
     sectionHeading2: "Events",
-    description2: "#semanticClimate is hosting a series of hackathons. Check out our Events page to find out when the next one is. (link to events page)"
+    description2:
+      "#semanticClimate is hosting a series of hackathons. Check out our Events page to find out when the next one is. (link to events page)",
   },
-  feed: { // used in feed.xml.njk
+  feed: {
+    // used in feed.xml.njk
     subtitle: "Liberating knowledge from climate-related reports",
     filename: "atom.xml",
     path: "/atom.xml",
     id: "https://semanticclimate.netlify.app/",
     authorName: "Semantic Climate",
-    authorEmail: ""
+    authorEmail: "",
   },
-  hero: { // used in hero section of main page ie. index.html.njk
+  hero: {
+    // used in hero section of main page ie. index.html.njk
     title: "#semanticClimate",
     description: "",
-  }
-}
+  },
+};
